7cad8dede152da4785cd28101e448d01037e0e3d
Robert McGibbon <rmcgibbo@gmail.com>
2012-11-09 17:17:07 -0800

    setup.py looks for CUDA in a much more flexivle way

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os
+from os.path import join as pjoin
 from setuptools import setup
 from distutils.unixccompiler import UnixCCompiler
 from distutils.extension import Extension
@@ -6,8 +7,39 @@ from distutils.command.build_ext import build_ext
 import subprocess
 from subprocess import CalledProcessError
 import glob
+import numpy
 
-subprocess.check_call('swig -python -c++ -o src/swig_wrap.cpp src/swig.i', shell=True)
+def find_in_path(name, path):
+   #adapted fom http://code.activestate.com/recipes/52224-find-a-file-given-a-search-path/
+   for dir in path.split(os.pathsep):
+      binpath = pjoin(dir, name)
+      if os.path.exists(binpath):
+         return os.path.abspath(binpath)
+   return None
+
+def locate_cuda():
+   # first check if the CUDAHOME env variable is in use
+   if 'CUDAHOME' in os.environ:
+      home = os.environ['CUDAHOME']
+      nvcc = pjoin(home, 'bin', 'nvcc')
+   else:
+      # otherwise, search the PATH for NVCC
+      nvcc = find_in_path('nvcc', os.environ['PATH'])
+      if nvcc is None:
+         raise EnvironmentError('The nvcc binary could not be '
+             'located in your $PATH. Either add it to your path, or set $CUDAHOME')
+      home = os.path.dirname(os.path.dirname(nvcc))
+
+   cudaconfig = {'home':home, 'nvcc':nvcc,
+                 'include': pjoin(home, 'include'),
+                 'lib64': pjoin(home, 'lib64')}
+   for k, v in cudaconfig.iteritems():
+      if not os.path.exists(v):
+         raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+
+   return cudaconfig
+
+CUDA = locate_cuda()
 
 # Obtain the numpy include directory.  This logic works across numpy versions.
 try:
@@ -15,7 +47,6 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
-# python distutils doesn't have NVCC by default. This is a total hack.
 
 class MyExtension(Extension):
     """subclass extension to add the kwarg 'glob_extra_link_args'
@@ -29,23 +60,15 @@ class MyExtension(Extension):
 class NVCC(UnixCCompiler):
     src_extensions = ['.cu']
     executables = {'preprocessor' : None,
-                   'compiler'     : ["nvcc"],
-                   'compiler_so'  : ["nvcc"],
-                   'compiler_cxx' : ["nvcc"],
-                   'linker_so'    : ["echo"], # TURN OFF NVCC LINKING
-                   'linker_exe'   : ["gcc"],
-                   'archiver'     : ["ar", "-cr"],
+                   'compiler'     : [CUDA['nvcc']],
+                   'compiler_so'  : [CUDA['nvcc']],
+                   'compiler_cxx' : [CUDA['nvcc']],
+                   # TURN OFF NVCC LINKING -- we're going to link with gcc instead
+                   'linker_so'    : ["echo"],
+                   'linker_exe'   : None,
+                   'archiver'     : None,
                    'ranlib'       : None,
-               }
-    def __init__(self):
-        # Check to ensure that nvcc can be located
-        try:
-            subprocess.check_output('nvcc --help', shell=True)
-        except CalledProcessError:
-            print >> sys.stderr, 'Could not find nvcc, the nvidia cuda compiler'
-            sys.exit(1)
-        UnixCCompiler.__init__(self)
-
+                   }
 
 # this code will get compiled up to a .o file by nvcc. the final .o file(s) that
 # it makes will be just one for each input source file. Note that we turned off
@@ -55,13 +78,13 @@ nvcc_compiled = Extension('this_name_is_irrelevant',
                           extra_compile_args=['-arch=sm_20', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'"],
                           # we need to include src as an input directory so that the header files and device_kernel.cu
                           # can be found
-                          include_dirs=['/usr/local/cuda/include', 'src'],
+                          include_dirs=[CUDA['include'], 'src'],
                           )
 
 # the swig wrapper for gpuaddr.cu gets compiled, and then linked to gpuaddr.o
 swig_wrapper = MyExtension('_gpuadder',
                          sources=['src/swig_wrap.cpp'],
-                         library_dirs=['/usr/local/cuda/lib64'],
+                         library_dirs=[CUDA['lib64']],
                          libraries=['cudart'],
                          include_dirs = [numpy_include],
                          # extra bit of magic so that we link this
@@ -103,6 +126,12 @@ class custom_build_ext(build_ext):
         
         # call superclass
         build_ext.build_extension(self, *args, **kwargs)
+
+
+if find_in_path('swig', os.environ['PATH']):
+   subprocess.check_call('swig -python -c++ -o src/swig_wrap.cpp src/swig.i', shell=True)
+else:
+   raise EnvironmentError('the swig executable was not found in your PATH')
 
 setup(name='gpuadder',
       # random metadata. there's more you can supploy
