f617e7e9ff65583904e39ec42c38901a325d7f15
Brendt Wohlberg <brendt@ieee.org>
2017-09-02 07:50:55 -0600

    Make setup.py compatible with Python 2 and 3

--- cython/setup.py
+++ cython/setup.py
@@ -1,3 +1,4 @@
+from future.utils import iteritems
 import  os
 from os.path import join as pjoin
 from setuptools import setup
@@ -41,7 +42,7 @@ def locate_cuda():
     cudaconfig = {'home':home, 'nvcc':nvcc,
                   'include': pjoin(home, 'include'),
                   'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.iteritems():
+    for k, v in iteritems(cudaconfig):
         if not os.path.exists(v):
             raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
 
